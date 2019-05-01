56e807b163b5f186143ce6595b0e4b9834c89c52
onahbosen <onahbosen@gmail.com>
2019-05-01 18:38:41 -0500

    almost done? gotta fix some bugs and do more styling

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+create database burgers_db;
+use burgers_db;
+create table burgers (
+    id int not null auto_increment,
+    burger_name varchar(30) not null,
+    devoured boolean default false,
+    date timestamp default now(),
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+insert into burgers (burger_name) values ('blucy');
+insert into burgers (burger_name) values ('classic');
+insert into burgers (burger_name) values ('bacon');
+insert into burgers (burger_name) values ('cease & desist');
+insert into burgers (burger_name) values ('merriam park');
+insert into burgers (burger_name) values ('baked n loaded');
+insert into burgers (burger_name) values ('horsekick');
+insert into burgers (burger_name) values ('islander');
+insert into burgers (burger_name) values ('mount blucuvious');
